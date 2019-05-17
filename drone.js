db03b3fe870097930d4e773ad898addd0032a472
Vesss <39167980+Vesss@users.noreply.github.com>
2019-05-17 11:29:30 +0100

    Add files via upload

---
+++ drone.js
@@ -0,0 +1,84 @@
+//const input = 'R+ R+- L+ L+-++---- L+++-+ R+++ R- R-- R+ R+-+-- R+--- R---++++--+- R++-+ L- R- R+ L++ R+ L+ R--- R+ L-+ L+ R+ L++-+ R- R+ R+--+ L- L-- L+ L+-+--+ L----+- R++ R-++------++-++ R-+ R++-- R- R+++--+ L--+-+ R--+-- L+ L- L+-+- R++ R-+-++++ L+ R+-+ L+-+++- R+ R--- L-----+---+ L- R- R+ R-+ R+ R- R+-- L- L+ L+ L- L+ R- L+ R- L+-+--+-+++-+ R+ L++ L- L--+ R- R+- R+ R--+ R-+ R- R--++-+ R- L+-- R- L- L- L--+-+-+-++-- L---+ L+ R+- L-+-+- L++- R+ L- L+- L+ L++ R++- R- R++ L+ R- R- R- L+ R-++ R+++ R- R---- R+--++- R++++-- L+ R- R- R++ L-+--+ L+ L-- R+ L-----+++--+++-+ L- L+-- L+ L- R+- R+ R--- L+ L+ L+ L- R- R++- R+ L++++-- L-+---+++-++--+ L--+- L+ R+ R+ L- R+ L+ R- R- L--++ L-+-++---++ R-+--+----++- L+ L--+++++-+- L-+++ L-+--+ L- R- L- L- R-+- R++-- R- L--+--++ R-+ L-+--++ L+-+ L+ R- L- R+ R- R-++-+- L+ L- R+--++ R+++-+ L++ R-+ R++ L+ R+ L+++++- L---- L+-- R+-+--++-++ L+ L+ L+- L+ R- R+ L+ R+ R+--- L--+ R- R+- R- R- R-+ R++++--+ R-+- R+ R--+ R- R+ R+ L- L++- R- R- L+ R+ R- R-+ L- L- R+ R-+ R+ L-++++ L-++---- R-+-- L--- R+-+-- R++- R- L- L- R+ R+++--+-+++++++- R-+-++- R+ L-+ L++-- R+-+ L- L- R- R+-+- R+ L- L++- R++--- L-+--- R+++ L- R- R+---++- L+ R- R+ R- R- R+ L----++- L-+---+ L- L- L++--++ L- L++-+- L+ R- L+- L--+------ R- L-+ R++- L- R+ L+ L-+++- R--+-+--+-+---+- R+ R- L+ L+- L-+--+++--+ L+- L++ L+ R-+- R+---+- L+- R-- L-++-+ L- L+++--- R+ R- L-- R---- L+ R- L+ L++ R+-+ R-+- L+ L- R+ R+-+- L- L- R-+ R+- L+ L+ L- L-+-+- R+- L+ L+++- L+ R+ L++--++++-- R+ R+ L++ R- L+ L- L--+++ L+ L+ L- L+-+ L- R-- R- L---++ L- R- L+ L--+--+ R- R+-+ R++ R- L+++ L+ R+-- R- R+-+- R++ L+-- R+ R+ L+ R- L+-+ R-++ L++----+-+- R+ R+ L+ L-- L- R++ L+ R-++--- R---- R- R- R+ L-- L-+- L--+-+ L- L- R+ R- L+++---+- L-+--+++-+-++- L+ L- L-++--+- L- R- R- L+ L- L+ L- L-++-- L- L+ R-++ R- L+ R- L+- R+++ R++ L+ R-+-+- R- R+ R- L+ R+ R- L+ L++ R-+++- L- L- R+ L-- R--+- L+ L-++ L+ L- L+- L+ R+-+ R++ R+ R--++ R-- R+ R- L+ R+ L+ R- L- L- L+ R+ R++ R+- R+ L-+ R+-- L+ L+ R+ R- R- L+ R+ L++ L- L-+-++ L- L- L-++-+ R++ L+ R+-- L- L- R- L+-++-- R+ L-+ R- R+ L+ R- L+ R-+- L- L++ L+ L--+- R--++- R+ R- R+ R+-- L- L+ L-+ R+ L- R-++ L+ R++ L- R- L-- L- L+-- R+ L- R+ L+ L+ L- R--+ R- R+ R+-+ R++++++-+--++ R---+ R+ R- R+ R- L++ R-- L- R+ R- R+ R++-+- R++ R+ L- R+-+ L+ L+ L++++ L- L+++- L++ L--+++ L-+ R- L-+++++-+--+- R+-++-+- L+ R- L- R-------+---- L+ R- L+-+++- R- R- R+++++--- R-+ L+ L--+ L- R- L-- L+ R++ R- R-- L---+- L+----+ L- L- R+- R+ L+ L+ R- L- R-+ L+ R+ L+-- R+- R- R+- R+-+---+ R+ L+ L- L+ L+ L--++--+ L+ L-+-- R+ L- L- R+ R+-+-++ L- L+ R- R+ L- L+- R- L+++-+ L- R-- L++ L--++ L+++ L+ L+- R++- L- L+ R+ R+ R++++++-+--+ L-++-+++ L- L- L+++ L- L++ R-+ R- R- L-+ L+ L+ R-----+++ R- R- L- L-++- L+ R- R+---++++---- L-- L- R++ L- R+ R- R+ R- R+ L+ R+-+-+++-+ R+- R- L+ R++++ R+ L+ L+++ R- R+ L---+ L- L-- L---+++-+++ L++ R+- R+---+-+ R+ R--+---- L-++ R+ R- L- R- R+ R+ L- R--+-+- L--+--+- L+ L++++-++ R-+-- L++- R+--+--++-+-+-- R+ L- R++ L--+-+++ L+ L- R-+-+- R- R- L- L-- L+-++--- L+ R+ R+ L+ L- L+ L+ L-+--- R+-- L+ L- R+ L++- R+---++ R+--+ R-- L- L+ L- L+--- L+-+ L- R-+--+ R++- L+ R+-+ R-+ L-+- R+ L- L- R- L- R- L++++ L++++-++ R-+- L- R- R- R+ R+ L- R++-+++----+ R+ R- R+ L- L--+ L- R- L+ L- L+-+-+-+ R+- L++ L-+-- L- L----- R+--+-++ L+- L- L-- R- L+-+- L-+ L- L++-++ R--+ R+ R-+-- L- R+ R---++ L--+ L+ L+ L++- R-+- L+ L+-+- R-- L-++--+ R- L+ R+ L-+ L---+ L- L+ R- L+ R- R- L+ R-----+--+ L- R+- L-++ L+ L- R-- L-++ L+ R+- L- L- L- L- R+ R+ R- R- L- R--++- L-+++ L- L+- L----- L- R+--+-- L--+-+ L-+ L++- R+ R- L---- L- L+ R-+ R+ L--+--+-++-- R- L+ R+ R- R- L--+++-++ L+ L-++- R+ L--++- L- L+ R+-+-+--- R+ L- R- L+ R+ L+- L- R++- R+ L--- R- L- R+-+++-+ R++---++ R--+----- R- R+ L- R+ R- R--- L+ R+ L+ L- L+ L- R+ R- L- L- L-+--+ R+ L+ L- R- R+ R+-++- L--+-- L- L- L- R- R+ L+-++ L- R- L-- L-+-- R- R--++ L+-+-- R+ L-+-+ L+---+ R-++- R-+++ R-++ R- L- L-++++ R+ L- R- R- L- R+ R- L- L- L+ R- R+ R+ L- R+ L- L+ L++ R+-+- R+-+- L- R-+- L-++ R- L+ R- R--+++---- R+- L- L++++--+ L++-+ R++- R+ R-+ L+- R- L++ R+ L- L- R++ L- R-+-+-- R+++-+ L- R-- R++++-++ L-- R++ R+-++-- R+-+- L-++++-+- R+ R+-- L++ R+++- L+- L+ L-- L- R+ R-++++-+ L- L+-+--++- L+ L+ R+ L--- L+ L- L-+- R+-- R+ R- L- L-+ R- R- L+ L+ R+ R- L- L+++ R+ R++++- R-++ R+ R-+-+ R-+++ R- L--+--++ R+ L++-+++ L+++ R--++-+-++--- R+ L+ R+--+- R- R- L--+ R+ L++ L-++ R- L+ R+++-++++++ L- R- R+ L+ R+-- L+--+ R- L-- R- R- R- L+ L- L++ L- L- R- R+ L- R++- L+ R+-+ L-+++ L+--+-- R-- L- R+ L+ L-++-+--++-+- L- R+ L- L-- R+ L+++ L- L+ R+ L- R--+ L+------+ R++';
+const input = 'R+ L-- R+ R-- L++';
+const steps = input.split(' ');
+
+let x = 0;
+let y = 0;
+
+// We assume we are heading North in the beginning
+let direction = 'N';
+let incrementX = 0;
+let incrementY = 0;
+
+for(const step of steps) {
+    const turn = step[0]; // Letter -> R or L
+    const movements = step.slice(1).split(''); // symbols array -> + or -
+
+    // Determine if we should increment/decrement X or increment/decrement Y and update the direction.
+    // The direction is needed to know what will be the new direction in the next iteration.
+    determineDirectionAndMovement(turn);
+
+    for(const moveSymbol of movements) {
+        let goForward = false;
+        if(moveSymbol === '+') {
+            goForward = true;
+        } else if(moveSymbol === '-') {
+            goForward = false;
+        } else {
+            throw new Error('Unknown move');
+        }
+
+        if((incrementX === 1 && goForward) || (incrementX === -1 && !goForward)) {
+            x = x + 1;
+        } else if((incrementX === 1 && !goForward) || (incrementX == -1 && goForward)) {
+            x = x - 1;
+        } else if((incrementY === 1 && goForward) || (incrementY === -1 && !goForward)) {
+            y = y + 1;
+        } else if((incrementY === 1 && !goForward) || (incrementY === -1 && goForward)) {
+            y = y - 1;
+        }
+    }
+}
+
+console.log(`(${x}, ${y})`);
+
+function determineDirectionAndMovement(turn) {
+    
+    if(turn !== 'R' && turn !== 'L') {
+        throw new Error('Unknown turn');
+    }
+
+    if(direction === 'N' && turn === 'R') { // If we are facing North and turning right, we will face East
+        incrementX = 1;
+        incrementY = 0;
+        direction = 'E';
+    } else if(direction === 'N' && turn === 'L') { // If we are facing North and turning left, we will face West
+        incrementX = -1;
+        incrementY = 0;
+        direction = 'W';
+    } else if(direction === 'E' && turn === 'R') { // If we are facing East and turning right, we will face South
+        incrementX = 0;
+        incrementY = -1;
+        direction = 'S';
+    } else if(direction === 'E' && turn === 'L') { // If we are facing East and turning left, we will face North
+        incrementX = 0;
+        incrementY = 1;
+        direction = 'N'
+    } else if(direction === 'S' && turn === 'R') { // If we are facing South and turning right, we will face West
+        incrementX = -1;
+        incrementY = 0;
+        direction = 'W';
+    } else if(direction === 'S' && turn === 'L') { // If we are facing South and turning left, we will face East
+        incrementX = 1;
+        incrementY = 0;
+        direction = 'E';
+    } else if(direction === 'W' && turn === 'R') { // If we are facing West and turning right, we will face North
+        incrementX = 0;
+        incrementY = 1;
+        direction = 'N';
+    } else if(direction === 'W' && turn === 'L') { // If we are facing West and turning left, we will face South
+        incrementX = 0;
+        incrementY = -1;
+        direction = 'S';
+    }
+}
